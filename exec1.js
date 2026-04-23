ed0070a3d311bb96b9b2bc293c5c3f470597f7d0
neto09dev09-star <neto09dev.09@gmail.com>
2026-04-23 13:31:13 -0300

    aula javascript
    inicio do assunto

---
+++ exec1.js
@@ -0,0 +1,6 @@
+let exemplo1 = "eu gosto de pao" // ou  "1"
+let exemplo2 =  1
+
+console.log(exemplo1 + exemplo1)
+
+console.log(exemplo2 + exemplo2)
--- index.js
+++
@@ -1,2 +0,0 @@
-const message = "Hello Word"
-console.log(index.js)
