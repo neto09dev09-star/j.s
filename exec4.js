ae8ee66af43418ff6a79b380169b83601aed18b5
neto09dev09-star <neto09dev.09@gmail.com>
2026-05-07 13:31:22 -0300

    atualização
    atividades

---
+++ exec4.js
@@ -0,0 +1,5 @@
+let idade= 18
+let temingresso= true
+let maioridade= true
+
+console.log ("tem ingresso?", temingresso && maioridade)
---
+++ exem.js
@@ -0,0 +1,4 @@
+let anoatual = 2026// ele é mutável 
+const datadenascimento= 2009 // nunca muda 
+
+console.log (anoatual-datadenascimento)
