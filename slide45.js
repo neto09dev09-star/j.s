65d53e19568a0a779eb0821a538833b053fba297
neto09dev09-star <neto09dev.09@gmail.com>
2026-05-14 12:12:33 -0300

    aula de hoje

---
+++ slide45.js
@@ -0,0 +1,7 @@
+let pontuacao = 120;
+
+let resultado = pontuacao >= 100 
+    ? "Você ganhou um prêmio!" 
+    : "Tente novamente";
+
+console.log(resultado);
---
+++ slide51.js
@@ -0,0 +1,11 @@
+let carro = {
+    modelo: "Civic",
+    ano: 2020,
+    automatico: true
+};
+
+console.log(
+    "O carro modelo " + carro.modelo +
+    " do ano " + carro.ano +
+    " é automático? " + carro.automatico
+);
